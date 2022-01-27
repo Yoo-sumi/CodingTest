667f7274fa155cfd49a1a8d9e611668bd3d13cb4
yoosumi <>
2022-01-27 20:59:20 +0900

    Docs: 구현 예제 4번 소스코드 추가

--- Implementation/gameDevelopment.py
+++ Implementation/gameDevelopment.py
@@ -1,74 +1,49 @@
-n, m = map(int, input().split())
-x, y, direction =list(map(int, input().split()))
-x-=1
-y-=1
+n,m=map(int,input().split())
+
+d=[[0]*m for _ in range(n)]
+x,y,direction=map(int,input().split())
+d[x][y]=1
+
 array=[]
-gone_list=[]
-for i in range(4):
-    array.append(list(map(int, input().split())))
-steps=0
+for i in range(n):
+    array.append(list(map(int,input().split())))
+# 북, 동, 남, 서 방향 정의
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
+
+def turn_left():
+    global direction
+    direction-=1
+    if direction==-1:
+        direction=3
+
+count=1
+turn_time=0
 while True:
-    count=0
-
-    for i in range(4):
-        if direction==0:
-            next_x=x
-            next_y=y-1
-        elif direction==1:
-            next_x=x-1
-            next_y=y
-        elif direction==2:
-            next_x=x
-            next_y=y+1
-        elif direction==3:
-            next_x=x+1
-            next_y=y
-        toggle=0 # 없다
-        print(gone_list)
-        for j in gone_list:
-            if (next_x>=0 and next_y>=0 and next_x<4 and next_y<4):
-                if str(next_x)+str(next_y)==j:
-                    #print("구 리스트에 있다")
-                    toggle=1
-                    break
-        if toggle==0:
-
-            if (next_x>=0 and next_y>=0 and next_x<4 and next_y<4):
-                if array[next_x][next_y]==1:
-                    x=next_x
-                    y=next_y
-                    gone_list.append(str(x)+str(y))
-                    steps+=1
-                    #print("pl")
-            else:
-                if direction==0:
-                    direction=3
-                else:
-                    direction-=1
-
-            count+=1
+    turn_left()
+    nx=x+dx[direction]
+    ny=y+dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny]==0 and array[nx][ny]==0:
+        d[nx][ny]=1
+        x=nx
+        y=ny
+        count+=1
+        turn_time=0
+        continue
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time+=1
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time==4:
+        nx=x-dx[direction]
+        ny=y-dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny]==0:
+            x=nx
+            y=ny
+        # 뒤가 바다로 막혀있는 경우
         else:
-            if direction==0:
-                direction=3
-            else:
-                direction-=1
-            count+=1
-    if count==4:
-        if direction==0:
-            next_x=x+1
-            next_y=y
-        elif direction==1:
-            next_x=x
-            next_y=y-1
-        elif direction==2:
-            next_x=x-1
-            next_y=y
-        elif direction==3:
-            next_x=x
-            next_y=y+1
-        if (next_x<0 and next_y<0 and next_x>3 and next_y>3):
-            if array[next_x][next_y]==0:
-                break
-print(steps)
-
-
+            break
+        turn_time=0
+print(count)
--- Implementation/gameDevelopment2.py
+++ Implementation/gameDevelopment2.py
@@ -1,9 +1,71 @@
+# 내가 작성한 코드
 n, m = map(int, input().split())
 x, y, direction =list(map(int, input().split()))
-x-=1
-y-=1
+steps=1
 array=[]
-gone_list=[]
+gone_list=[[0]*m for i in range(n)]
+gone_list[x][y]=1
 for i in range(4):
     array.append(list(map(int, input().split())))
 
+def search_direction():
+    if direction==0:
+        new_x=x-1
+        new_y=y
+    elif direction==1:
+        new_x=x
+        new_y=y+1
+    elif direction==2:
+        new_x=x+1
+        new_y=y
+    elif direction==3:
+        new_x=x
+        new_y=y-1
+    return  new_x,new_y
+def change_direction():
+    global direction
+    if direction==0:
+        direction=3
+    else:
+        direction-=1
+def back_direction():
+    if direction==0:
+        new_x=x+1
+        new_y=y
+    elif direction==1:
+        new_x=x
+        new_y=y-1
+    elif direction==2:
+        new_x=x-1
+        new_y=y
+    elif direction==3:
+        new_x=x
+        new_y=y+1
+    return new_x,new_y
+
+count=0
+while True:
+    change_direction()
+    new_x,new_y=search_direction()
+    if gone_list[new_x][new_y]==0 and array[new_x][new_y]==0:
+        x=new_x
+        y=new_y
+        gone_list[x][y]=1
+        count=0
+        steps+=1
+    else: count+=1
+
+    if count==4:
+        new_x,new_y=back_direction()
+        if array[new_x][new_y]==0:
+            x=new_x
+            y=new_y
+            count=0
+            print("뒤로 감 ")
+            print(str(x)+str(y)+str(direction))
+        else:
+            print(str(x)+str(y)+str(direction))
+            break
+    print(str(x)+str(y)+str(direction))
+print(steps)
+print(gone_list)
