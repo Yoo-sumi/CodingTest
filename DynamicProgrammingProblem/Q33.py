88818db6f09d5c927842597fbef637701e86ebb4
yoosumi <>
2022-02-25 00:42:10 +0900

    no message

--- DynamicProgrammingProblem/Q33.py
+++ DynamicProgrammingProblem/Q33.py
@@ -1,24 +1,24 @@
 '''n=7
 day=[3,5,1,1,2,4,2]
 money=[10,20,10,20,15,40,200]'''
-n=10
+'''n=10
 day=[1,1,1,1,1,1,1,1,1,1]
-money=[1,2,3,4,5,6,7,8,9,10]
-d=[0]*(n)
+money=[1,2,3,4,5,6,7,8,9,10]'''
+'''n=10
+day=[5,5,5,5,5,5,5,5,5,5]
+money=[10,9,8,7,6,10,9,8,7,6]'''
+n=10
+day=[5,4,3,2,1,1,2,3,4,5]
+money=[50,40,30,20,10,10,20,30,40,50]
 
-for i in range(n):
-    now=i
-    next=i+day[i]
-    if d[now]==0 and next<n:
-        d[now]=money[now]
-    while True:
-        print(now,next)
-        if next<n and next+day[next]<n:
-            d[next]=max(d[next],d[now]+money[next])
-            now=next
-            next=now+day[now]
-            print(d)
-        else:
-            break
+d=[0]*(n+1)
+max_value=0
+for i in range(n-1,-1,-1):
+    time=day[i]+i
+    if time<=n:
+        d[i]=max(money[i]+d[time],max_value)
+        max_value=d[i]
+    else:
+       d[i]=max_value
 print(max(d))
 
